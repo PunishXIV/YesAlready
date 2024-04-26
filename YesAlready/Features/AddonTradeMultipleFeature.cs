49291f71e5c58463ffc35afa2004f67dbd3a1bff
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2024-04-26 10:10:09 +0200

    fix trademultiple being always active

--- YesAlready/Features/AddonTradeMultipleFeature.cs
+++ YesAlready/Features/AddonTradeMultipleFeature.cs
@@ -1,6 +1,7 @@
 using Dalamud.Game.Addon.Lifecycle;
 using Dalamud.Game.Addon.Lifecycle.AddonArgTypes;
 using Dalamud.Memory;
+using ECommons;
 using ECommons.Automation;
 using FFXIVClientStructs.FFXIV.Component.GUI;
 using YesAlready.BaseFeatures;
@@ -22,11 +23,14 @@ internal class AddonTradeMultipleFeature : BaseFeature
 
     protected static unsafe void AddonUpdate(AddonEvent eventType, AddonArgs addonInfo)
     {
-        if (!P.Active)
+        if (!P.Active || !P.Config.TradeMultiple)
             return;
 
         var addon = (AtkUnitBase*)addonInfo.Addon;
         if (MemoryHelper.ReadSeStringNullTerminated(new nint(addon->AtkValues[0].String)).ToString() != "5/5")
+            return;
+
+        if (GenericHelpers.TryGetAddonByName<AtkUnitBase>("SelectYesno", out var _))
             return;
 
         Callback.Fire(addon, true, 0);
