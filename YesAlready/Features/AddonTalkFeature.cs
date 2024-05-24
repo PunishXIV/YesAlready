8c19b7e7e62ba1c7b839d6c9926b72c5f996f675
Cat <blackcatstone@proton.me>
2024-05-24 20:36:31 +0100

    Make the matched state latch when (and only when) you talk to a different NPC

--- YesAlready/Features/AddonTalkFeature.cs
+++ YesAlready/Features/AddonTalkFeature.cs
@@ -28,6 +28,8 @@ internal class AddonTalkFeature : BaseFeature
 
     private ClickTalk? clickTalk = null;
     private IntPtr lastTalkAddon = IntPtr.Zero;
+    // private string lastTalkTarget = string.Empty;
+    private bool matched = false;
 
     protected unsafe void AddonUpdate(AddonEvent eventType, AddonArgs addonInfo)
     {
@@ -35,11 +37,6 @@ internal class AddonTalkFeature : BaseFeature
 
         if (!P.Active || addon == null || !GenericHelpers.IsAddonReady(addon))
             return;
-
-        var target = Svc.Targets.Target;
-        var targetName = P.LastSeenTalkTarget = target != null
-            ? Utils.SEString.GetSeStringText(target.Name)
-            : string.Empty;
 
         if ((P.ForcedYesKeyPressed && !P.Config.SeparateForcedKeys) || P.ForcedTalkKeyPressed)
         {
@@ -50,20 +47,36 @@ internal class AddonTalkFeature : BaseFeature
             return;
         }
 
-        var nodes = P.Config.GetAllNodes().OfType<TalkEntryNode>();
-        foreach (var node in nodes)
-        {
-            if (!node.Enabled || string.IsNullOrEmpty(node.TargetText))
-                continue;
+        var target = Svc.Targets.Target;
+        var targetName = target != null
+            ? Utils.SEString.GetSeStringText(target.Name)
+            : string.Empty;
 
-            var matched = EntryMatchesTargetName(node, targetName);
-            if (!matched)
-                continue;
+        if (targetName != P.LastSeenTalkTarget) {
+            Svc.Log.Debug("Target Name: " + targetName + ", lastTalkTarget: " + P.LastSeenTalkTarget);
+            P.LastSeenTalkTarget = targetName;
+            matched = false;
 
+            var nodes = P.Config.GetAllNodes().OfType<TalkEntryNode>();
+
+            foreach (var node in nodes)
+            {
+                if (!node.Enabled || string.IsNullOrEmpty(node.TargetText))
+                    continue;
+                
+                matched = EntryMatchesTargetName(node, targetName);
+
+                if (matched) {
+                    Svc.Log.Debug("Talk match seen: " + matched + " Node: " + node.Name);
+                    break;    
+                }
+            }
+        }
+
+        if ( matched ) {
+            Svc.Log.Debug("AddonTalk: Advancing");
             if (clickTalk == null || lastTalkAddon != (IntPtr)addon)
                 clickTalk = ClickTalk.Using(lastTalkAddon = (IntPtr)addon);
-
-            Svc.Log.Debug("AddonTalk: Advancing");
             clickTalk.Click();
             return;
         }
