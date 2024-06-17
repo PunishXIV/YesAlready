9616daa003a35bbd2996fd4e736544fa0d3f826d
Cat <blackcatstone@proton.me>
2024-06-17 22:34:32 +0100

    Evaluate the list of Talk matches less often (#124)
    * Make the matched state latch when (and only when) you talk to a different NPC

--- YesAlready/Features/AddonTalkFeature.cs
+++ YesAlready/Features/AddonTalkFeature.cs
@@ -28,6 +28,8 @@ internal class AddonTalkFeature : BaseFeature
 
     private ClickTalk? clickTalk = null;
     private IntPtr lastTalkAddon = IntPtr.Zero;
+    private string lastTalkTarget = string.Empty;
+    private bool matched = false;
 
     protected unsafe void AddonUpdate(AddonEvent eventType, AddonArgs addonInfo)
     {
@@ -50,20 +52,31 @@ internal class AddonTalkFeature : BaseFeature
             return;
         }
 
-        var nodes = P.Config.GetAllNodes().OfType<TalkEntryNode>();
-        foreach (var node in nodes)
-        {
-            if (!node.Enabled || string.IsNullOrEmpty(node.TargetText))
-                continue;
+        if (targetName != lastTalkTarget) {
+            Svc.Log.Debug("Target Name: " + targetName + ", lastTalkTarget: " + lastTalkTarget);
+            lastTalkTarget = targetName;
+            matched = false;
 
-            var matched = EntryMatchesTargetName(node, targetName);
-            if (!matched)
-                continue;
+            var nodes = P.Config.GetAllNodes().OfType<TalkEntryNode>();
 
+            foreach (var node in nodes)
+            {
+                if (!node.Enabled || string.IsNullOrEmpty(node.TargetText))
+                    continue;
+                
+                matched = EntryMatchesTargetName(node, targetName);
+
+                if (matched) {
+                    Svc.Log.Debug("Talk match seen: " + matched + " Node: " + node.Name);
+                    break;    
+                }
+            }
+        }
+
+        if ( matched ) {
+            Svc.Log.Debug("AddonTalk: Advancing");
             if (clickTalk == null || lastTalkAddon != (IntPtr)addon)
                 clickTalk = ClickTalk.Using(lastTalkAddon = (IntPtr)addon);
-
-            Svc.Log.Debug("AddonTalk: Advancing");
             clickTalk.Click();
             return;
         }
