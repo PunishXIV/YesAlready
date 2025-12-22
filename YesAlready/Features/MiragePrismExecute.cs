9ad3908745d6075d416e1948ba55deb2a4922d4b
Leontopodium Nivale <103514618+LeontopodiumNivale14@users.noreply.github.com>
2025-12-22 16:53:57 -0500

    Temp AM for glamour apply

--- YesAlready/Features/MiragePrismExecute.cs
+++ YesAlready/Features/MiragePrismExecute.cs
@@ -6,4 +6,16 @@ internal class MiragePrismExecute : AddonFeature
     protected override bool IsEnabled() => C.MiragePrismExecuteCast;
 
     protected override unsafe void HandleAddonEvent(AddonEvent eventType, AddonArgs addonInfo, AtkUnitBase* atk) => new AddonMaster.MiragePrismExecute(atk).Cast();
+
+    public partial class AddonMaster // TODO: wait for EC to merge
+    {
+        public unsafe class MiragePrismExecute : AddonMasterBase<AtkUnitBase>
+        {
+            public MiragePrismExecute(nint addon) : base(addon) { }
+            public MiragePrismExecute(void* addon) : base(addon) { }
+            public unsafe AtkComponentButton* CastButton => Addon->GetComponentButtonById(23);
+            public void Cast() => ClickButtonIfEnabled(CastButton);
+            public override string AddonDescription { get; } = "Cast glamour window";
+        }
+    }
 }
